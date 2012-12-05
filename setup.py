36f2b165ac73ad5795e243aada0cec16ce940178
Theo Julienne <theo@icy.com.au>
2012-12-06 10:52:28 +1100

    Add initial instructions to the README

--- setup.py
+++ setup.py
@@ -2,21 +2,26 @@ import os
 from setuptools import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "SandboxTemplating",
-    version = "0.0.1",
-    author = "Theo Julienne",
-    author_email = "theo@magmalabs.com.au",
-    description = ("A flexible templating library using a real language grammer and AST visitors."),
-    license = "MIT",
-    keywords = "templating",
-    url = "",
-    packages=['SandboxTemplating'],
-    long_description=read('README'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: MIT License",
-    ],
+	name = "SandboxTemplating",
+	version = "0.0.1",
+	author = "Theo Julienne",
+	author_email = "theo@magmalabs.com.au",
+	description = ("A flexible templating library using a real language grammer and AST visitors."),
+	license = "MIT",
+	keywords = "templating",
+	url = "",
+	packages=['SandboxTemplating'],
+	long_description=read('README'),
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"License :: OSI Approved :: MIT License",
+	],
+	install_requires=[
+		'MarkupSafe',
+		'PythonInterfaces',
+		'parcon',
+	]
 )
